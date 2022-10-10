b60116cf98b1f0e989b8052fab7b57162210a5ed
chen <807477447@qq.com>
2022-10-10 21:43:01 +0800

    将Login模块的SecurityAutoConfiguration禁用

--- Login/src/main/java/com/clouddrive/login/LoginApplication.java
+++ Login/src/main/java/com/clouddrive/login/LoginApplication.java
@@ -2,12 +2,12 @@ package com.clouddrive.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.clouddrive")
+@SpringBootApplication(scanBasePackages = "com.clouddrive", exclude = {SecurityAutoConfiguration.class})
 public class LoginApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class, args);
     }
-
 }
